e976479c24d2829328547972d0ee401ab7ba2ed3
Leon Do <leondo123@gmail.com>
2022-12-24 14:10:31 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ 
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent,
+  ] 
+});
+
+client.on("messageCreate", message => {
+  if (message.content === "ping") {
+    message.reply("pong")
+  }
+})
+
+client.login(process.env.DISCORD_SECRET)
